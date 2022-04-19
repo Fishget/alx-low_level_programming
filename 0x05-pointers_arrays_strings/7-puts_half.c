ad32951ccef5bc273a681f3e55824b62927d64d5
Fishget <fishget1@gmail.com>
2022-04-19 07:58:42 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,15 +5,18 @@
  *
  *Return: void which means our answer is correct
  */
+
 void puts_half(char *str)
 {
 int i, last;
+
 i = 0;
 while (str[i] != '\0')
 {
 i++;
 }
-last = (i = 1) / 2;
+
+last = (i + 1) / 2;
 for (i = last; str[i]; i++)
 {
 _putchar (str[i]);
