87da9f513100e1f83ad2c7be140a4482984dd72e
Fishget <fishget1@gmail.com>
2022-04-19 07:21:01 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,10 +4,10 @@
  *puts - prints every character of a string
  *@str: A pointer to an int that will changed
  *
- *Return: Void which means it is correct
+ *Return: void which means it is correct
  */
 
-void puts_half(char *str);
+void puts2(char *str)
 {
 int a;
 
