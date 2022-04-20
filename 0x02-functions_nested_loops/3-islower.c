a8ee3be35c6479b8d553f5e938c52b1055b74f72
Fishget <fishget1@gmail.com>
2022-04-20 08:38:41 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * add -prints add two numbers
- *@i: print int i
- *@k: print int k
+ * _islower - Short description, single line
+ * @c: contains value to be compared
  * Return: Always 0.
  */
+int _islower(int c)
 
-int add(int i, int k)
 {
-	return (i + k);
+
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
 }
