412268c97a68b50924606700c9e8fca2fbbe36dc
Fishget <fishget1@gmail.com>
2022-04-20 01:45:54 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-    * main - Prints combination of numbers
-      *
-        * Return: Always (Success)
-	  */
+ * main - Prints combination of numbers
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 		int c, i;
 
-			for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 					{
-								for (i = '0'; i <= '9'; i++)
+for (i = '0'; i <= '9'; i++)
 											{
-															if (c < i)
+if (c < i)
 																			{
 																								putchar(c);
 																												putchar(i);
 
-																																if (c != '8' || (c == '8' && i != '9'))
+if (c != '8' || (c == '8' && i != '9'))
 																																					{
 																																											putchar(',');
 																																																putchar(' ');
